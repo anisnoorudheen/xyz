e5bfcef05c17e6d9dec284803423f12c7efbaa68
Rizky Benggolo <travarilo@gmail.com>
2019-02-07 10:51:52 +0700

    X00T: Add RetroMusic
    Change-Id: Ie456a89feb1d8db1e207731792eb6d2eca4b8293

--- device.mk
+++ device.mk
@@ -418,6 +418,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Recorder
 
+# RetroMusic
+PRODUCT_PACKAGES += \
+    RetroMusic
+
 # Seccomp
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
